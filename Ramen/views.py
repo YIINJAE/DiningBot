b638c3ae72a915b77c034f82615acd0c5483b5f7
YIINJAE <ijlee@abiman.com>
2024-09-30 17:31:58 +0900

    Feat(login):로그인 및 유저 사용 추가
     - 로그인 구성 추가
     - 로그인하여 결재 정보 추가 하여 db생성 추가

--- Ramen/views.py
+++ Ramen/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+#메인 화면 첫화면
+def index(request):
+    return render(request, './index.html')
